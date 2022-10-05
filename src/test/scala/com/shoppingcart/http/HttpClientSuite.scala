3185976d8230cdb901eba716cf6d9bf043a61413
Gang Wang <gang.gwgs@gmail.com>
2022-10-05 19:40:51 -0400

    Refactor HttpClient to make response handler testable

--- src/test/scala/com/shoppingcart/http/HttpClientSuite.scala
+++ src/test/scala/com/shoppingcart/http/HttpClientSuite.scala
@@ -1,27 +1,42 @@
 package com.shoppingcart.http
 
 import cats.effect.IO
-import com.shoppingcart.service.Logger
 import munit.CatsEffectSuite
-import org.http4s.Uri
+import org.http4s.{ Status, Uri, Response => Http4sResponse }
 
 object HttpClientSuite {
   val successResp = Response("Cornflakes", 1.99)
   implicit val SuccessHttpClientIO: HttpClient[IO] = new HttpClient[IO] {
-    def call(url: Uri)(implicit logger: Logger[IO]): IO[Either[String, Response]] = IO { Right(successResp) }
+    def call(url: Uri): IO[Either[String, Response]] = IO { Right(successResp) }
 
     def error(message: String): IO[Either[String, Response]] = IO { Left(message) }
   }
 
   val errorMsg = "404 not found"
   implicit val FailedHttpClientIO: HttpClient[IO] = new HttpClient[IO] {
-    def call(url: Uri)(implicit logger: Logger[IO]): IO[Either[String, Response]] = IO { Left(errorMsg) }
+    def call(url: Uri): IO[Either[String, Response]] = IO { Left(errorMsg) }
 
     def error(message: String): IO[Either[String, Response]] = IO { Left(message) }
   }
 }
 
 class HttpClientSuite extends CatsEffectSuite {
+  import HttpClient.syncEntityJsonEncoder
+
+  test("response handler") {
+    val response = Response("bread", 1.99)
+    val httpResp = Http4sResponse[IO](Status.Ok).withEntity(response)
+    HttpClient.respHandler[IO](httpResp).map {
+      result => assertEquals(result, Right(response))
+    }
+  }
+
+  test("response handler for empty") {
+    val emptyResp = Http4sResponse[IO]()
+    HttpClient.respHandler[IO](emptyResp).map {
+      result => assertEquals(result, Left("Malformed message body: Invalid JSON: empty body"))
+    }
+  }
 
   test("get price successfully") {
     import HttpClientSuite.SuccessHttpClientIO
